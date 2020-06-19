afb7d2cf73d3ec549dfeb2f759341157e3c54793
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-06-19 22:32:38 +0900

    6/19 xml

--- app/src/main/java/com/example/memo_01/MainActivity.java
+++ app/src/main/java/com/example/memo_01/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.memo_01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview);
+
     }
 }

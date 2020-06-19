8272cabefbde44821a381bd87a1f461ad1a7ac99
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-06-20 02:38:03 +0900

    6/20 recyclerview

--- app/src/main/java/com/example/memo_01/MainActivity.java
+++ app/src/main/java/com/example/memo_01/MainActivity.java
@@ -1,20 +1,107 @@
 package com.example.memo_01;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    RecyclerAdapter recyclerAdapter;
+
+    List<Memo> memoList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.recyclerview);
+        memoList = new ArrayList<>();
+        memoList.add(new Memo("test1", "testtext",0));
+        memoList.add(new Memo("test2", "testtext",0));
+        memoList.add(new Memo("test3", "testtext",0));
+        memoList.add(new Memo("test4", "testtext",1));
+        memoList.add(new Memo("test5", "testtext",1));
 
+
+        recyclerView = findViewById(R.id.recyclerview);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        recyclerAdapter = new RecyclerAdapter(memoList);
+        recyclerView.setAdapter(recyclerAdapter);
+
+    }
+
+    class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder> {
+
+        private List<Memo> listdata;
+
+        public RecyclerAdapter(List<Memo> listdata) {
+            this.listdata = listdata;
+        }
+
+        @NonNull
+        @Override
+        public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item, viewGroup, false);
+            return new ItemViewHolder(view);
+        }
+
+        @Override
+        public int getItemCount() {
+            return listdata.size();
+        }
+
+        // 데이터를 레이아웃에 어떻게 넣어 줄지 정함
+        @Override
+        public void onBindViewHolder(@NonNull ItemViewHolder itemViewHolder, int i) {
+            Memo memo = listdata.get(i);
+
+            itemViewHolder.maintext.setText(memo.getMaintext());
+            itemViewHolder.subtext.setText(memo.getSubtext());
+
+            if (memo.getIsdone() == 0) {
+                itemViewHolder.img.setBackgroundColor(Color.LTGRAY);
+            }else {
+                itemViewHolder.img.setBackgroundColor(Color.GREEN);
+            }
+        }
+
+        // 리스트 추가/제거
+        void addItem(Memo memo) {
+            listdata.add(memo);
+        }
+        void removeItem(int position) {
+            listdata.remove(position);
+        }
+
+        class ItemViewHolder extends RecyclerView.ViewHolder {
+
+            private TextView maintext;
+            private TextView subtext;
+            private ImageView img;
+
+            public ItemViewHolder(@NonNull View itemView) {
+                super(itemView);
+
+                maintext = itemView.findViewById(R.id.item_main_text);
+                subtext = itemView.findViewById(R.id.item_sub_text);
+                img = itemView.findViewById(R.id.item_image);
+
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/memo_01/Memo.java
@@ -0,0 +1,38 @@
+package com.example.memo_01;
+
+public class Memo {
+
+    String maintext;
+    String subtext;
+    int isdone;
+
+    public Memo(String maintext, String subtext, int isdone) {
+        this.maintext = maintext;
+        this.subtext = subtext;
+        this.isdone = isdone;
+    }
+
+    public String getMaintext() {
+        return maintext;
+    }
+
+    public void setMaintext(String maintext) {
+        this.maintext = maintext;
+    }
+
+    public String getSubtext() {
+        return subtext;
+    }
+
+    public void setSubtext(String subtext) {
+        this.subtext = subtext;
+    }
+
+    public int getIsdone() {
+        return isdone;
+    }
+
+    public void setIsdone(int isdone) {
+        this.isdone = isdone;
+    }
+}
